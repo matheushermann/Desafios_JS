5f92f15fd50fb9451294ae91043768d70d4e1fea
matheushermann0 <matheushermann0@gmail.com>
2026-06-01 21:45:41 -0300

    Desafio02

---
+++ Desafio02/media.js
@@ -0,0 +1,18 @@
+//Desafio 02 - média
+/* 
+  Crie um array com 5 notas
+  - Utilize um loop para:
+       - Somar todas as notas
+       - Calcular a média
+
+    Mostrar no console/output o seguinte:
+    Média da turma: 10.0
+*/
+
+
+ let notas = [10.0, 8.5, 7.5, 6.0, 9.0]; 
+
+ let somar = 0;
+ for(resultado = 0; resultado <= notas.length; resultado++ ) {
+ somar =+ + notas[resultado];
+ }
