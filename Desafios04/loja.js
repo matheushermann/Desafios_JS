c52235091894815e8c75ec6b03bdc575e43d5224
matheushermann0 <matheushermann0@gmail.com>
2026-06-15 21:45:52 -0300

    Desafio04

---
+++ Desafios04/loja.js
@@ -0,0 +1,13 @@
+/*
+DESAFIO 04 - LOJA TECH
+ Uma loja de informatica possui varios produtos cadastrados.
+
+ Crie um array de objetos contendo:
+ - nome
+ - preco
+
+ Utilize uma estrutura de repetição para mostrar apenas 
+ os produtos que custam mais de R$ 100,00
+
+ Exiba o nome dos produtos encontrados.
+ */
